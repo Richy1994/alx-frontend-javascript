d5d7c6219d399ca48132584dd7f9c975cade6200
Richy1994 <indomitablerichy@gmail.com>
2023-09-27 21:34:55 +0000

    1-block-scoped.js

---
+++ 1-block-scoped.js
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+    <html>
+        <bod>
+        export default function taskBlock(trueOrFalse){
+            var task = false;
+            var task2 = true;
+        
+
+            if (trueOrFalse){
+    var task = true;
+    var task2 = false;
+            }
+    return [task, task2];
+            }
+<script>
+document.getElementById("demo").innerHTML =
+"The value of z is: " + var;
+</script>
+        </bod>
+    </html>
